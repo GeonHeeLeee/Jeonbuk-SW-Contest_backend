45533bac9d197ee8ff1503edb8c31f2bbc41dde3
GeonHeeLeee <ghlee00125@naver.com>
2024-03-11 16:38:27 +0900

    feat: FavoriteRestaurant Entity 생성 및 연관관계 설정

---
+++ contest/Jeonbuk-SW-Contest-backend/contest/src/main/java/Jeonbuk/contest/entity/FavoriteRestaurant.java
@@ -0,0 +1,30 @@
+package Jeonbuk.contest.entity;
+
+import Jeonbuk.contest.entity.restaurant.Restaurant;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+public class FavoriteRestaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    //Member가 해당 식당에 대한 Comment를 달 수 있도록
+    private String comment;
+
+    //유저 별점
+    @Size(min = 1, max = 5)
+    private int rating;
+}
