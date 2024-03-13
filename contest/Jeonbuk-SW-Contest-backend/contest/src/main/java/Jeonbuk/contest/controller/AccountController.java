d27efdb0ab75c84101e0f4f9943d7d74bcd05d4f
GeonHeeLeee <ghlee00125@naver.com>
2024-03-13 15:57:40 +0900

    feat: registerInfo 로직 작성

--- contest/Jeonbuk-SW-Contest-backend/contest/src/main/java/Jeonbuk/contest/controller/AccountController.java
+++ contest/Jeonbuk-SW-Contest-backend/contest/src/main/java/Jeonbuk/contest/controller/AccountController.java
@@ -2,6 +2,7 @@ package Jeonbuk.contest.controller;
 
 import Jeonbuk.contest.domain.MemberInfoDTO;
 import Jeonbuk.contest.domain.MemberRegisterDTO;
+import Jeonbuk.contest.exception.ErrorDTO;
 import Jeonbuk.contest.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -13,13 +14,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/account")
 @RequiredArgsConstructor
 @Tag(name = "로그인/회원가입", description = "로그인 및 회원가입 관련 API")
@@ -35,8 +34,7 @@ public class AccountController {
                             examples = @ExampleObject(name = "회원가입 응답 예시", value = "{\"memberId\": \"value\"}")
                     ))})
     @PostMapping("/register")
-    @ResponseBody
-    public ResponseEntity<Map> registerUser(@RequestBody MemberRegisterDTO memberRegisterDTO) {
+    public ResponseEntity<Object> registerUser(@RequestBody MemberRegisterDTO memberRegisterDTO) {
         String memberId = accountService.registerMember(memberRegisterDTO);
         return ResponseEntity.ok(Collections.singletonMap("memberId", memberId));
     }
@@ -46,17 +44,18 @@ public class AccountController {
             @ApiResponse(responseCode = "200", description = "중복 아이디 존재하지 않음", content = @Content(schema = @Schema(hidden = true))),
             @ApiResponse(responseCode = "409", description = "중복 아이디 존재", content = @Content(schema = @Schema(hidden = true)))})
     @GetMapping("/register/{memberId}")
-    public ResponseEntity checkDuplicateId(@Parameter(description = "검사할 ID") @PathVariable(value = "memberId") String memberId) {
+    public ResponseEntity<String> checkDuplicateId(@Parameter(description = "검사할 ID") @PathVariable(value = "memberId") String memberId) {
         return accountService.checkDuplicateId(memberId);
     }
 
     @Operation(summary = "이름, 전화번호, 비상연락망 등록")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "정보 등록 성공", content = @Content(schema = @Schema(hidden = true)))
+            @ApiResponse(responseCode = "200", description = "정보 등록 성공", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", description = "해당 ID의 사용자 존재하지 않음", content = @Content(schema = @Schema(implementation = ErrorDTO.class)))
 
     })
     @PostMapping("/register/info")
-    public ResponseEntity registerInfo(@RequestBody MemberInfoDTO memberInfoDTO) {
+    public ResponseEntity<MemberInfoDTO> registerInfo(@RequestBody MemberInfoDTO memberInfoDTO) {
         return accountService.registerMemberInfo(memberInfoDTO);
     }
 }
