9a75d3b475e540784527827a3a811347b4744c3d
GeonHeeLeee <ghlee00125@naver.com>
2024-03-14 10:20:25 +0900

    feat: checkDuplicateId Test 작성

--- contest/Jeonbuk-SW-Contest-backend/contest/src/test/java/Jeonbuk/contest/controller/AccountControllerTest.java
+++ contest/Jeonbuk-SW-Contest-backend/contest/src/test/java/Jeonbuk/contest/controller/AccountControllerTest.java
@@ -10,13 +10,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,7 +41,7 @@ public class AccountControllerTest {
     @Test
     @DisplayName("회원가입 테스트")
     @WithMockUser("member")
-    void registerUser() throws Exception {
+    void registerUserTest() throws Exception {
         given(accountService.registerMember(new MemberRegisterDTO("testId", "testPassword")))
                 .willReturn("testId");
 
@@ -59,5 +62,37 @@ public class AccountControllerTest {
                 .andDo(print());
 
         verify(accountService).registerMember(new MemberRegisterDTO("testId", "testPassword"));
+    }
+
+    @Test
+    @DisplayName("중복 ID 성공 테스트")
+    @WithMockUser("member")
+    void checkDuplicateIdSuccessTest() throws Exception {
+        given(accountService.checkDuplicateId("existingMember"))
+                .willReturn(new ResponseEntity(HttpStatus.OK));
+
+        String memberId = "existingMember";
+
+        mockMvc.perform(get("/account/register/" + memberId))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        verify(accountService).checkDuplicateId(memberId);
+    }
+
+    @Test
+    @DisplayName("중복 ID 실패 테스트")
+    @WithMockUser("member")
+    void checkDuplicateIdFailTest() throws Exception {
+        given(accountService.checkDuplicateId("notExistingMember"))
+                .willReturn(new ResponseEntity(HttpStatus.CONFLICT));
+
+        String memberId = "notExistingMember";
+
+        mockMvc.perform(get("/account/register/" + memberId))
+                .andExpect(status().isConflict())
+                .andDo(print());
+
+        verify(accountService).checkDuplicateId(memberId);
     }
 }
