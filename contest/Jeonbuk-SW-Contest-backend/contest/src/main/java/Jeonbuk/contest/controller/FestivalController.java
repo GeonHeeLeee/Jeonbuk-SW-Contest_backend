e4967e8d5616fb7d0d370f6b79f81b4904ded180
GeonHeeLeee <ghlee00125@naver.com>
2024-05-14 10:21:13 +0900

    feat: 축제 북마크 등록 로직 작성

--- contest/Jeonbuk-SW-Contest-backend/contest/src/main/java/Jeonbuk/contest/controller/FestivalController.java
+++ contest/Jeonbuk-SW-Contest-backend/contest/src/main/java/Jeonbuk/contest/controller/FestivalController.java
@@ -1,20 +1,24 @@
 package Jeonbuk.contest.controller;
 
+import Jeonbuk.contest.domain.BookmarkDTO;
 import Jeonbuk.contest.entity.DiscountStore;
 import Jeonbuk.contest.entity.Festival;
+import Jeonbuk.contest.exception.ErrorDTO;
 import Jeonbuk.contest.service.BookmarkService;
 import Jeonbuk.contest.service.FestivalService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "축제", description = "page 번호 처음엔 0, 이후 pageable의 PageNumber + 1로 요청")
 @Slf4j
@@ -30,6 +34,19 @@ public class FestivalController {
     @GetMapping("/list/all")
     public ResponseEntity<Page<Festival>> getAllFestivalPage(@Parameter(description = "페이지 번호") @RequestParam(value = "page") int page) {
         return festivalService.getAllFestivalPage(page);
+    }
+
+    @Operation(summary = "축제 즐겨찾기 등록", description = "memberId: 사용자ID, typeId: 해당 할인매장의 Id, bookmarkType: RESTAURANT(식당), DISCOUNT_STORE(할인매장), FESTIVAL(축제), TOWN_STROLL(동네 마실)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "북마크 등록 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(name = "북마크 Id", value = "{\"bookmarkId\": \"value\"}")
+                    ))})
+    @ApiResponse(responseCode = "400", description = "북마크 중복 등록/BookmarkType 요청 오류", content = @Content(schema = @Schema(implementation = ErrorDTO.class)))
+    @PostMapping("/bookmark")
+    public ResponseEntity<?> bookmarkFestival(@RequestBody BookmarkDTO bookmarkDTO) {
+        return bookmarkService.registerBookmark(bookmarkDTO);
     }
 
 }
